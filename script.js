aaac444c3d3e159aedab9fdda0b614da47e6034e
JP Brunache <34013885+kosmik7@users.noreply.github.com>
2024-10-23 11:29:15 +0200

    Add the logic for a single round

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let computerScore = 0
+let humanChoice = 0
+
 function getComputerChoice() {
     randomNumber = Math.floor(Math.random() * 3)
 
@@ -14,7 +17,7 @@ function getComputerChoice() {
 function getHumanChoice() {
     let userInput = prompt('rock, paper or scissors?');
     userInput = userInput.toLowerCase().replace(/[^a-z]/g, "");
-    
+
     if (userInput === 'rock' || userInput === 'paper' || userInput === 'scissors') {
         return userInput;
     } else {
@@ -23,3 +26,18 @@ function getHumanChoice() {
     }
 }
 
+function playRound(computerChoice, humanChoice) {
+    if (computerChoice === humanChoice) {
+        return `It's a tie! You both threw ${humanChoice}`;
+    } else if (
+        (computerChoice === 'rock' && humanChoice === 'scissors') ||
+        (computerChoice === 'paper' && humanChoice === 'rock') ||
+        (computerChoice === 'scissors' && humanChoice === 'paper')
+    ) { 
+        return `You lose! ${computerChoice} beats ${humanChoice}.`;
+    } else {
+        return `You won! ${humanChoice} beats ${computerChoice}.`;
+    }
+}
+
+console.log(playRound(getComputerChoice(), getHumanChoice()))
